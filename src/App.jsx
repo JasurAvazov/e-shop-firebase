c9912605ddaa0d7a8fa62b7be504d625e2bdf172
jasur avazov <avazovjasur95@gmail.com>
2023-10-13 12:34:14 +0500

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/header";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!!</h1>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </>
   );
 }
 
